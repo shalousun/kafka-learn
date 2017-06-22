68bead4fbfeed0555fe988c7d7dd4f3b390afe4c
oppofind <836575280@qq.com>
2017-06-22 23:54:49 +0800

    add kafka client api test

---
+++ SimpleKafka/README.md
@@ -0,0 +1,1 @@
+本demo使用kafka的简单api,参考自api文档https://kafka.apache.org/0100/javadoc/index.html?org/apache/kafka/clients/consumer/KafkaConsumer.html
---
+++ SimpleKafka/src/main/java/com/sunyu/kafka/SimpleConsumer.java
@@ -0,0 +1,34 @@
+package com.sunyu.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * 消费者
+ * Created by yu on 2017/6/22.
+ */
+public class SimpleConsumer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.248.128:9092,192.168.248.130:9092,192.168.248.133:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        //订阅主题
+        consumer.subscribe(Arrays.asList("jsa-kafka-topic"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+        }
+    }
+}
